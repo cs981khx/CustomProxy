fc4ba3cb8eb05371abe9966aa212714a922c1cd4
K H <khizercomm@yahoo.com>
2017-08-23 15:32:31 +0100

    Initial committ

---
+++ CustomProxy/ProxyAuthorisation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+
+namespace CustomProxy
+{
+    public class ProxyAuthorisation : IWebProxy
+    {
+        ICredentials crendential = new NetworkCredential("DOMAIN\\username", "password");
+
+        public ICredentials Credentials
+        {
+            get
+            {
+                return crendential;
+            }
+            set
+            {
+                crendential = value;
+            }
+        }
+
+        public Uri GetProxy(Uri destination)
+        {
+            return new Uri(@"proxyURL", UriKind.Absolute);
+        }
+
+        public bool IsBypassed(Uri host)
+        {
+            return host.IsLoopback;
+        }
+
+    }
+}
